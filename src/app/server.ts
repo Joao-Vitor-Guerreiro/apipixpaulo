c3ef68c7bcd7a923a32b5cf215be76c929a7b779
ruyteer <ruyteraraujo992@gmail.com>
2025-04-19 23:34:43 -0300

    fix: add cors

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import { router } from "./routes/routes";
+import cors from "cors";
+
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.listen(3434, () => {
   console.log("Server is running on 3333...");
