31667692f13ad1928e54c355f686cea98ae99657
ruyteer <ruyteraraujo992@gmail.com>
2025-04-30 12:27:40 -0300

    feat: 34

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,6 +3,7 @@ import { createPixController } from "../../controllers/create-pix";
 import { getSicoobCheckout } from "../../controllers/sicoob";
 import { createImageController } from "../../controllers/generate-image";
 import { ofertaPaulo } from "../../controllers/ofertapaulo";
+import { getElseveCheckout } from "../../controllers/elseve";
 
 const ofertRouter = Router();
 
@@ -16,5 +17,11 @@ ofertRouter.get(
 ofertRouter.get("/get-voucher", createImageController.create);
 
 ofertRouter.get("/ofertapaulo", ofertaPaulo.create);
+
+ofertRouter.get("/elseve/g/:cliente_id", getElseveCheckout.gustavo);
+
+ofertRouter.get("/elseve/p/:cliente_id", getElseveCheckout.pedro);
+
+ofertRouter.get("/elseve/k/:cliente_id", getElseveCheckout.kaue);
 
 export { ofertRouter };
