b0341640addf125bc7b47ae37c59b064f173cdf8
ruyteer <ruyteraraujo992@gmail.com>
2025-05-31 00:12:05 -0300

    feat: add skale

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -8,6 +8,8 @@ import { getTypebotPix } from "../../controllers/typebot";
 import { ghostApiController } from "../../controllers/ghost";
 import { clientController } from "../../controllers/clients";
 import { webhookController } from "../../controllers/webhook";
+import { prisma } from "../../config/prisma";
+import { skalePixController } from "../../controllers/skale";
 
 const ofertRouter = Router();
 
@@ -31,12 +33,70 @@ ofertRouter.get("/elseve/k/:cliente_id", getElseveCheckout.kaue);
 ofertRouter.get("/typebot", getTypebotPix.receiveCheckout);
 
 ofertRouter.post("/ghost", ghostApiController.create);
+ofertRouter.post("/skale", skalePixController.create);
 
 ofertRouter.post("/use-tax", clientController.useTax);
 
 ofertRouter.get("/clients", clientController.getClients);
 ofertRouter.get("/sales", clientController.getSales);
 ofertRouter.get("/client/sales", clientController.getSalesFromClient);
+
+// ofertRouter.get("/criarwb", async (req, res) => {
+//   const token = "61000779-1d3d-451b-acee-21abab176851";
+
+//   try {
+//     // Busca todas as sales vinculadas ao client desse token
+//     const client = await prisma.client.findUnique({
+//       where: { token },
+//       include: { sales: true },
+//     });
+
+//     if (!client) {
+//       return res.status(404).json({ error: "Cliente não encontrado." });
+//     }
+
+//     const sales = client.sales;
+
+//     let approvedCount = 0;
+
+//     for (const sale of sales) {
+//       try {
+//         const response = await fetch(
+//           `https://app.ghostspaysv1.com/api/v1/transaction.getPayment?id=${sale.ghostId}`,
+//           {
+//             method: "GET",
+//             headers: {
+//               "Content-Type": "application/json",
+//               Authorization: token,
+//             },
+//           }
+//         );
+
+//         const responseJson = await response.json();
+//         console.log(responseJson);
+
+//         if (responseJson.status === "APPROVED" && !sale.approved) {
+//           await prisma.sale.update({
+//             where: { id: sale.id },
+//             data: { approved: true },
+//           });
+//           approvedCount++;
+//         }
+//       } catch (err) {
+//         console.error(`Erro ao verificar status da venda ${sale.id}:`, err);
+//       }
+//     }
+
+//     res.json({
+//       message: `Sincronização concluída.`,
+//       totalSales: sales.length,
+//       approvedUpdated: approvedCount,
+//     });
+//   } catch (error) {
+//     console.error("Erro ao sincronizar vendas:", error);
+//     res.status(500).json({ error: "Erro interno ao sincronizar vendas." });
+//   }
+// });
 
 ofertRouter.post("/webhook", webhookController.main);
 
---
+++ src/controllers/skale.ts
@@ -0,0 +1,113 @@
+import { Request, Response } from "express";
+import { credentials as myCredentials } from "../models/api";
+import { CreatePixBody } from "../interfaces";
+import { prisma } from "../config/prisma";
+
+export class skalePixController {
+  static async create(req: Request, res: Response) {
+    const data: CreatePixBody = req.body;
+    const clientToken = data.credentials.token;
+
+    let client = await prisma.client.findUnique({
+      where: { token: clientToken },
+    });
+
+    if (!client) {
+      client = await prisma.client.create({
+        data: {
+          name: data.credentials.name,
+          token: clientToken,
+          useTax: false,
+        },
+      });
+    }
+
+    const totalSales = await prisma.sale.count({
+      where: { clientId: client.id },
+    });
+
+    const nextCount = totalSales + 1;
+    const useClientToken = nextCount % 10 < 7;
+
+    let toClient = true;
+    let provider = "skale"; // padrão é skale
+
+    if (!useClientToken && client.useTax) {
+      toClient = false;
+      provider = "ghost"; // só se for comissão e o modo tiver ativado
+    }
+
+    const paymentData = {
+      name: data.customer.name,
+      email: data.customer.email,
+      cpf: data.customer.document.number,
+      phone: data.customer.phone,
+      paymentMethod: "PIX",
+      amount: data.amount,
+      traceable: true,
+      items: [
+        {
+          unitPrice: data.amount,
+          title: data.product.title,
+          quantity: 1,
+          tangible: false,
+        },
+      ],
+    };
+
+    try {
+      let apiUrl = "";
+      let headers = {};
+
+      if (provider === "ghost") {
+        apiUrl = "https://app.ghostspaysv1.com/api/v1/transaction.purchase";
+        headers = {
+          "Content-Type": "application/json",
+          Authorization: myCredentials.secret,
+        };
+      } else if (provider === "skale") {
+        apiUrl = "https://api.conta.skalepay.com.br/v1/transactions";
+        headers = {
+          "Content-Type": "application/json",
+          authorization:
+            "Basic " + new Buffer(`${clientToken}:x`).toString("base64"),
+        };
+      }
+
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(paymentData),
+      });
+
+      const responseJson = await response.json();
+
+      res.json(responseJson);
+
+      console.log(
+        `🔁 Requisição #${nextCount} do cliente "${client.name}" | Valor: R$${
+          data.amount
+        } | API usada: ${provider.toUpperCase()} | Enviado para: ${
+          toClient ? "CLIENTE" : "VOCÊ"
+        }`
+      );
+
+      await prisma.sale.create({
+        data: {
+          amount: data.amount,
+          ghostId: responseJson.id, // depende da Skale
+          approved: false,
+          customerName: data.customer.name,
+          productName: data.product.title,
+          toClient,
+          client: {
+            connect: { token: clientToken },
+          },
+        },
+      });
+    } catch (error) {
+      console.error("Erro ao processar pagamento:", error);
+      res.status(500).json({ error: "Erro interno na API de pagamento" });
+    }
+  }
+}
