0dc8a47f7865f5fb88b8bae864fe227055fa3026
ruyteer <ruyteraraujo992@gmail.com>
2025-06-13 17:18:56 -0300

    fix: res.josn

--- src/controllers/getTransactionData.ts
+++ src/controllers/getTransactionData.ts
@@ -6,7 +6,7 @@ export class getTransactionScalarData {
       const { id } = req.params;
 
       const response = await fetch(
-        `https://app.pluggou.io/api/payments/transactions/${id}`,
+        `https://app.pluggou.io/api/payments/transactions?organizationId=X35AtBs7YNeBlGvoFa1NT`,
         {
           headers: {
             "X-API-Key": "pk_live_VC8yyiBfwWmtxcC3EbVPhip_D_HxMKtK",
@@ -18,9 +18,9 @@ export class getTransactionScalarData {
 
       const responseJson = await response.json();
 
-      console.log(responseJson);
+      const filtered = responseJson.data.filter((result) => result.id === id);
 
-      res.json(responseJson);
+      res.json(filtered[0]);
     } catch (error: any) {
       console.error("Erro em useTax:", error);
       res.status(500).json({ error: "Erro interno", details: error.message });
